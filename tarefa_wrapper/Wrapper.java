5e94c969d2784b2b591005ec0931b3346ba67c61
Marco Paiva <marcopaiva2004@gmail.com>
2024-05-13 09:15:55 +0200

    tarefa_wrapper

---
+++ tarefa_wrapper/Wrapper.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author marco
+ */
+public class Wrapper {
+
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Digite um número inteiro: ");
+            int meuInt = sc.nextInt();
+            Integer meuInteger = Integer.valueOf(meuInt);
+
+            System.out.println("\nValor do Integer: " + meuInteger);
+        }
+    }
+}
