d151e09eecfaed84b03268e4b98d6357acba8646
Marco Paiva <marcopaiva2004@gmail.com>
2024-05-17 12:03:48 +0200

    tarefa do módulo 17 - Generics

---
+++ tarefa_m17/cars/Honda.java
@@ -0,0 +1,8 @@
+package cars;
+
+public class Honda extends Car{
+
+    public Honda(String model, String color, int year) {
+        super(model, color, year);
+    }
+}
---
+++ tarefa_m17/cars/Opel.java
@@ -0,0 +1,8 @@
+package cars;
+
+public class Opel extends Car{
+
+    public Opel(String model, String color, int year) {
+        super(model, color, year);
+    }
+}
