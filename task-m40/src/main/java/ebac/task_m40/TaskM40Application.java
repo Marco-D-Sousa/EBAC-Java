99c40d2a6629448cecb2bbd95c9e3f4f4389ef06
Marco Sousa <marcopaiva2004@gmail.com>
2024-07-22 20:44:56 +0200

    task modulo 40

---
+++ task-m40/src/main/java/ebac/task_m40/TaskM40Application.java
@@ -0,0 +1,13 @@
+package ebac.task_m40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskM40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskM40Application.class, args);
+	}
+
+}
---
+++ task-m40/src/test/java/ebac/task_m40/TaskM40ApplicationTests.java
@@ -0,0 +1,13 @@
+package ebac.task_m40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskM40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
