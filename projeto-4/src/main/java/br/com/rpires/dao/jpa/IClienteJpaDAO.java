81f3eb2333b52230f2dfc012a1af6a74652af89b
Marco de Sousa <marcopaiva2004@gmail.com>
2024-09-24 14:18:13 +0200

    Finalizando o projeto 4

---
+++ projeto-4/src/main/java/br/com/rpires/dao/jpa/IClienteJpaDAO.java
@@ -0,0 +1,7 @@
+package br.com.rpires.dao.jpa;
+
+import br.com.rpires.dao.generic.jpa.IGenericJpaDAO;
+import br.com.rpires.domain.jpa.ClienteJpa;
+
+public interface IClienteJpaDAO extends IGenericJpaDAO<ClienteJpa, Long> {
+}
