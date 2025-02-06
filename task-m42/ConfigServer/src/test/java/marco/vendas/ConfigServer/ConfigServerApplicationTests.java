de90fb853ccd95b8f8be0980bba411f2a1e05c56
Marco Sousa <marcopaiva2004@gmail.com>
2025-02-06 20:13:54 +0100

    Iniciando a Tarefa do mod-42

---
+++ task-m42/ConfigServer/src/test/java/marco/vendas/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package marco.vendas.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ task-m42/ProdutoService/src/test/java/marco/vendas/ProdutoService/ProdutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package marco.vendas.ProdutoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
